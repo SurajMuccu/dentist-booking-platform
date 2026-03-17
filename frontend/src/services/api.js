470929b948e8542a5daba4e4b0488367faffad82
Suraj Muccu <surajchinni188@gmail.com>
2026-03-17 16:11:50 +0530

    connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api"
+const API_BASE = "https://dentist-booking-platform-wh8p.onrender.com"
 
 export const getDentists = async () => {
   const res = await fetch(`${API_BASE}/dentists`)
